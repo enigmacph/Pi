6f0437d3dadd20703c3dc06c778836655754b8d5
MperorM <mathiaskbonde@gmail.com>
2023-04-01 21:27:06 +0200

    lulz

--- code_red.py
+++ code_red.py
@@ -1,20 +1,25 @@
 import soco
+import schedule
 import time
 
 den = soco.SoCo('192.168.50.198')
 bedroom = soco.SoCo('192.168.50.82')
 hallway = soco.SoCo('192.168.50.33')
 
-# Create a new group with den as the coordinator
-master = den
-bedroom.join(master)
-hallway.join(master)
-
 # start 'code red' sequence on the group
 def start_code_red():
-    master.play_uri("http://192.168.50.252/etminut.mp3")
+    # Create a new group with den as the coordinator
+    master = den
+    bedroom.join(master)
+    hallway.join(master)
+    
+    # play alarm sound
+    master.play_uri("http://192.168.50.252/code_red_alarm.mp3")
+
+# Schedule start_code_red() to run at a certain time every day
+schedule.every().day.at("11:00").do(start_code_red)
 
 # Run the script indefinitely, calling start_code_red() every minute
 while True:
-    start_code_red()
-    time.sleep(10)
+    schedule.run_pending()
+    time.sleep(60)
