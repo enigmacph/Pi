079e2c327d1daf8934129dc119c7be70c81029ce
MperorM <mathiaskbonde@gmail.com>
2023-04-02 00:18:57 +0200

    rewrote as server

--- infoscreen/main.js
+++ infoscreen/main.js
@@ -1,34 +1,74 @@
+const http = require('http');
 const fs = require('fs');
 const path = require('path');
-const electron = require('electron');
-const { app, BrowserWindow } = electron;
+const socketIO = require('socket.io');
 
-let mainWindow;
-let htmlFiles = [];
-
-function createWindow() {
-  mainWindow = new BrowserWindow({
-    kiosk: true,
-    fullscreen: true,
-    frame: false
-  });
-
-  // Load the first file in the list
-  mainWindow.loadFile(htmlFiles[0]);
-
-  // Set up an interval to cycle through the files
-  let index = 1;
-  setInterval(() => {
-    mainWindow.loadFile(htmlFiles[index]);
-    index = (index + 1) % htmlFiles.length;
-  }, 10000);
-}
-
-// Read the contents of the HTML folder and store the filenames in an array
-fs.readdirSync(path.join(__dirname, 'HTML')).forEach(file => {
-  if (path.extname(file) === '.html') {
-    htmlFiles.push(path.join(__dirname, 'HTML', file));
+// Create an HTTP server
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    fs.readFile(path.join(__dirname, 'welcome.html'), (err, data) => {
+      if (err) {
+        res.writeHead(500);
+        res.end('Error loading welcome.html');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+      }
+    });
+  } else {
+    res.writeHead(404);
+    res.end('Not found');
   }
 });
 
-app.on('ready', createWindow);
+const io = socketIO(server);
+
+function getHtmlFilesFromFolder(folderPath) {
+  return new Promise((resolve, reject) => {
+    fs.readdir(folderPath, (err, files) => {
+      if (err) {
+        reject(err);
+      } else {
+        const htmlFiles = files.filter(file => path.extname(file) === '.html');
+        resolve(htmlFiles);
+      }
+    });
+  });
+}
+
+let currentFileIndex = 0;
+
+async function getNextHtmlFile(folderPath) {
+  const files = await getHtmlFilesFromFolder(folderPath);
+  const nextFile = path.join(folderPath, files[currentFileIndex]);
+  currentFileIndex = (currentFileIndex + 1) % files.length;
+  return nextFile;
+}
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  setInterval(async () => {
+    try {
+        const nextHtmlFile = await getNextHtmlFile(path.join(__dirname, 'HTML'));
+
+
+      fs.readFile(nextHtmlFile, (err, data) => {
+        if (err) {
+          console.error('Error reading next HTML file:', err);
+        } else {
+          socket.emit('updateContent', data.toString());
+        }
+      });
+    } catch (err) {
+      console.error('Error getting next HTML file:', err);
+    }
+  }, 5 * 60 * 10); // Update every 5 minutes
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
