2a9153590bb09d9a47b7981021c293370b164518
MperorM <mathiaskbonde@gmail.com>
2023-04-02 11:22:24 +0200

    FOOOOK

--- sound_manager.py
+++ sound_manager.py
@@ -9,18 +9,21 @@ hallway = soco.SoCo('192.168.50.33')
 # start 'code red' sequence on the group
 def play_sound(filename):
     # Create a new group with den as the coordinator
-    master = den
-    bedroom.join(master)
-    hallway.join(master)
+    try:
+        master = den
+        bedroom.join(master)
+        hallway.join(master)
+    except:
+        print("Error joining speakers")
     
     # play alarm sound
     master.play_uri(f"http://192.168.50.252/{filename}.mp3")
 
 # Schedule sounds
 # Note that raspberry pi thinks its GMT
-schedule.every().day.at("19:00").do(lambda: play_sound("pizzajingle"))
-schedule.every().day.at("12:00").do(lambda: play_sound("gas"))
-schedule.every().day.at("12:20").do(lambda: play_sound("gas"))
+schedule.every().day.at("17:00").do(lambda: play_sound("pizzajingle"))
+schedule.every().day.at("10:00").do(lambda: play_sound("gas"))
+schedule.every().day.at("10:24").do(lambda: play_sound("gas"))
 
 # Run the script indefinitely looking for scheduled sounds
 while True:
