2c8d4261e74273e6547fa45220e7ec26ab56d1ca
MperorM <mathiaskbonde@gmail.com>
2023-06-27 11:51:34 +0200

    lul

--- sound_manager.py
+++ sound_manager.py
@@ -30,6 +30,7 @@ def play_sound(filename):
 schedule.every().day.at("17:00").do(lambda: play_sound("pizzajingle"))
 schedule.every().day.at("10:00").do(lambda: play_sound("gas"))
 schedule.every().day.at("20:00").do(lambda: play_sound("ohyeah"))
+schedule.every().day.at("8:00").do(lambda: play_sound("japan_lul"))
 
 # Run the script indefinitely looking for scheduled sounds
 while True:
