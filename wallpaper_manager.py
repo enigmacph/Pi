6ee925479627c336b0609e97450d1b288fddedb5
MperorM <36660799+MperorM@users.noreply.github.com>
2024-08-24 17:42:00 +0200

    wallpaper_manager

--- wallpaper_manager.py
+++ wallpaper_manager.py
@@ -1,13 +1,9 @@
 import os
-import time
 import random
 import subprocess
 
 # Path to the folder containing wallpapers
 wallpaper_folder = '/home/pi/wallpapers'
-
-# Time interval between wallpaper changes (in seconds)
-interval = 60  # 60 = 1 minute
 
 def set_wallpaper(image_path):
     # Set environment variables to access the graphical session
@@ -17,20 +13,15 @@ def set_wallpaper(image_path):
     # Command to change the wallpaper in Raspberry Pi OS
     subprocess.run(['pcmanfm', '--set-wallpaper', image_path])
 
-while True:
-    # Get a list of all image files in the folder
-    wallpapers = [f for f in os.listdir(wallpaper_folder) if f.endswith(('.png', '.jpg', '.jpeg'))]
-        
-    if not wallpapers:
-        print("No images found in the specified folder.")
-        break
-        
+# Get a list of all image files in the folder
+wallpapers = [f for f in os.listdir(wallpaper_folder) if f.endswith(('.png', '.jpg', '.jpeg'))]
+    
+if not wallpapers:
+    print("No images found in the specified folder.")
+else:
     # Randomly choose an image
     selected_wallpaper = random.choice(wallpapers)
     wallpaper_path = os.path.join(wallpaper_folder, selected_wallpaper)
-        
+    
     # Set the chosen wallpaper
     set_wallpaper(wallpaper_path)
-        
-    # Wait for the specified interval before changing the wallpaper again
-    time.sleep(interval)
