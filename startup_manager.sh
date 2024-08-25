5d3d6bd2966e8bfc945a063d9389c3042dba6049
MperorM <36660799+MperorM@users.noreply.github.com>
2024-08-25 23:35:36 +0200

    ??

--- startup_manager.sh
+++ startup_manager.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo /usr/bin/python3 /home/pi/Python/Pi/sound_manager.py &
+sudo /usr/bin/python3 /home/pi/Python/Pi/soundmanager.py &
 sudo /usr/bin/python3 /home/pi/Python/Pi/experiments/motion_sensor.py &
 sudo /usr/bin/python3 /home/pi/Python/Pi/experiments/temp_sensor.py &
