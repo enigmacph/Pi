9a90009d05ac8af5b35e3da52064db9081fcc98b
MperorM <36660799+MperorM@users.noreply.github.com>
2024-08-26 21:57:55 +0200

    hello

--- experiments/remote.py
+++ experiments/remote.py
@@ -5,18 +5,9 @@ import time
 GPIO.setmode(GPIO.BCM)  # Use BCM numbering
 GPIO.setup(14, GPIO.IN)  # Set GPIO pin 14 as input (You can change this to any GPIO pin you are using)
 
-try:
-    print("KY-022 IR Receiver Test")
-    print("Waiting for IR signal...")
-
-    while True:
-        if GPIO.input(14) == 0:  # Assuming the module outputs LOW when an IR signal is detected
-            print("IR signal detected!")
-            time.sleep(0.2)  # Add a small delay to avoid overwhelming the console with messages
-        else:
-            time.sleep(0.1)  # Add a small delay to prevent excessive CPU usage
-
-except KeyboardInterrupt:
-    print("Program terminated")
-finally:
-    GPIO.cleanup()  # Clean up GPIO on exit
+while True:
+    if GPIO.input(14) == 0:  # Assuming the module outputs LOW when an IR signal is detected
+        print("IR signal detected!")
+        time.sleep(0.2)  # Add a small delay to avoid overwhelming the console with messages
+    else:
+        time.sleep(0.1)  # Add a small delay to prevent excessive CPU usage
--- livescreen/livescreen.py
+++ livescreen/livescreen.py
@@ -1,1 +1,3 @@
 #first commit
+
+# hej ehj
